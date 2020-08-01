f87671b1c46f32917ce36e5f03abaf4750af9c0a
blank <mdietz314@gmail.com>
2020-08-01 14:35:25 +0200

    -Made the landing page more intuitive and attractive -Beautified the design of the tictactoe game -Added some filling content
    --> Project done

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,66 @@
-console.log('JS FUNKT!');
+console.log('Java Script is working!');
 
-function toggleNav() {
-  var x = document.getElementById("nav");
-  if (x.style.display === "flex") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "flex";
+// notification bar toogle and animation
+$(function() {
+  setTimeout(function() {
+    return $(".notification-bar").animate({
+      height: "toggle"
+    }, "fast")
+  }, 500);
+  return $("#ok").on("click", function() {
+    $("#barwrap").css("margin-bottom", "0px");
+    $(".notification-bar").animate({
+      height: "toggle"
+    }, "fast");
+    return !1
+  })
+});
+
+// Hamburger Button onclick
+$(function() {
+  return $(".toggle-button").on("click", function() {
+    $("#nav").animate({
+      height: "toggle"
+    }, "fast");
+    return !1
+  })
+});
+
+// Product Description onclick
+$(function() {
+  return $(".product").on("click", function() {
+    $(this).find(".product__text").animate({
+      height: "toggle"
+    }, "fast");
+    return !1
+  })
+});
+
+/*
+// active class for nav bar onclick
+function addActive (active_id) {
+  var navArr = document.getElementsByClassName('nav-btn')
+  for (var i=0; i<navArr.length; i++) {
+    if (navArr[i].id != active_id) {
+      navArr[i].classList.remove("active-nav");
+    }
+    else {
+      navArr[i].classList.add("active-nav");
+    }
   }
+  console.log("addActive ausgeführt");
 }
+
+// Get all buttons with class="btn" inside the container
+var btns = document.getElementsByClassName("nav-btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active-nav");
+    current[0].className = current[0].className.replace(" active-nav", "");
+    this.className += " active-nav";
+    console.log("hat gefunkt");
+  });
+}
+*/
--- scripts/tictactoe_script.js
+++ scripts/tictactoe_script.js
@@ -1,45 +1,14 @@
-
 $(document).ready(function() {
 
+  // field array to check the winning combinations in winCheck()
   var fields = [
     [" ", " ", " "],
     [" ", " ", " "],
     [" ", " ", " "]
   ];
 
-// reset button
-  $("#btn").click(function() {
-    reset();
-    $(".field").each(function() {
-      $(this).text("")
-    });
-  });
-// reset function
-  function reset() {
-    fields = [
-      [" ", " ", " "],
-      [" ", " ", " "],
-      [" ", " ", " "]
-    ];
-  }
-
-// enemy function
-  function O() {
-    //Now a random field will be selected
-    var x = Math.floor(Math.random() * fields.length());
-    var y = Math.floor(Math.random() * fields.length());
-    var randomField = fields[x][y];
-    if (randomField == " ") {
-
-    }
-    else {
-      O();
-    }
-  }
-
-//checks if a player has won
   function winCheck() {
-
+    // checks all fieldarray win combinations
     if (fields[0][0] == fields[0][1] && fields[0][0] == fields[0][2] && fields[0][0] != " ") {
       return true;
     } else if (fields[1][0] == fields[1][1] && fields[1][0] == fields[1][2] && fields[1][0] != " ") {
@@ -59,59 +28,139 @@ $(document).ready(function() {
     }
 
   }
-// Click function
-// Player sets a X and then the PC sets a random O
-// main function
-  $(".field").click(function() {
-    if ($(this).text() == "") {
-        $(this).text("X");
-        if ($(this).attr('id') == "topL") {
-          console.log("0,0: Array wurde gesetzt");
-          fields[0][0] = "X";
-        }
-        if ($(this).attr('id') == "topM") {
-          console.log("0,1: Array wurde gesetzt");
-          fields[0][1] = "X";
-        }
-        if ($(this).attr('id') == "topR") {
-          console.log("0,2: Array wurde gesetzt");
-          fields[0][2] = "X";
-        }
-        if ($(this).attr('id') == "midL") {
-          console.log("1,0: Array wurde gesetzt");
-          fields[1][0] = "X";
-        }
-        if ($(this).attr('id') == "midM") {
-          console.log("1,1: Array wurde gesetzt");
-          fields[1][1] = "X";
-        }
-        if ($(this).attr('id') == "midR") {
-          console.log("1,2: Array wurde gesetzt");
-          fields[1][2] = "X";
-        }
-        if ($(this).attr('id') == "bottomL") {
-          console.log("2,0: Array wurde gesetzt");
-          fields[2][0] = "X";
-        }
-        if ($(this).attr('id') == "bottomM") {
-          console.log("2,1: Array wurde gesetzt");
-          fields[2][1] = "X";
-        }
-        if ($(this).attr('id') == "bottomR") {
-          console.log("2,2: Array wurde gesetzt");
-          fields[2][2] = "X";
-        }
 
-        if (winCheck()) {
-          alert("X hat gewonnen!");
-          reset();
-          $(".field").each(function() {
-            $(this).text("")
-          });
+  function drawCheck() {
+    // loops through the fieldarray and checks whether every field
+    // has been marked
+    for(var i = 0; i < fields.length; i++) {
+      var field = fields[i];
+      for(var j = 0; j < field.length; j++) {
+        if(field[j] == "X" || field[j] == "O");
+        else {
+          return false;
         }
-
       }
+    }
+    return true;
+  }
+
+  // reset function
+  function reset() {
+    fields = [
+      [" ", " ", " "],
+      [" ", " ", " "],
+      [" ", " ", " "]
+    ];
+  }
+
+  // checks which field was clicked and adds the player to the fieldarray
+  function setField(field, player) {
+
+    if (field.attr('id') == "topL") {
+      fields[0][0] = player;
+    }
+    if (field.attr('id') == "topM") {
+      fields[0][1] = player;
+    }
+    if (field.attr('id') == "topR") {
+      fields[0][2] = player;
+    }
+    if (field.attr('id') == "midL") {
+      fields[1][0] = player;
+    }
+    if (field.attr('id') == "midM") {
+      fields[1][1] = player;
+    }
+    if (field.attr('id') == "midR") {
+      fields[1][2] = player;
+    }
+    if (field.attr('id') == "bottomL") {
+      fields[2][0] = player;
+    }
+    if (field.attr('id') == "bottomM") {
+      fields[2][1] = player;
+    }
+    if (field.attr('id') == "bottomR") {
+      fields[2][2] = player;
+    }
+  }
+
+  function generateRandomField() {
+    // selects a random field by generating a random index over
+    // the .field class
+    var len = $(".field").length;
+    var random = Math.floor( Math.random() * len );
+    var randomField = $(".field").eq(random);
+
+    // checks wether the field is already marked
+    // if not recursive call
+    if (!randomField.hasClass("x") && !randomField.hasClass("o")) {
+      return randomField;
+    }
+    else {
+      return generateRandomField();
+    }
+  }
+
+  // Click function
+  // Player sets a X and then the PC sets a O
+  $(".field").click(function() {
+    // Checks wether the field has been clicked
+    if (!$(this).hasClass("x") && !$(this).hasClass("o")) {
+      // Marks the field
+      $(this).addClass("x");
+
+      // Adds the X to the fieldarray
+      setField($(this),"X");
+
+      if (winCheck()) {
+        $(".winning-message-text").text("Congratulations! Put in your E-Mail to get the discount code:");
+        $("#mail").show();
+        $(".winning-message").addClass("show");
+        return;
+      }
+      else {
+          if(drawCheck()) {
+            $(".winning-message-text").text("Draw!");
+            $("#mail").hide();
+            $(".winning-message").addClass("show");
+            return;
+          }
+      }
+
+      // Now the it's the PCs turn
+      var oField = generateRandomField();
+      // Marks the field
+      oField.addClass("o");
+      // Adds the O to the fieldarray
+      setField(oField, "O");
+
+
+      if (winCheck()) {
+        $(".winning-message-text").text("Oh no ;(");
+        $("#mail").hide();
+        $(".winning-message").addClass("show");
+        return;
+      }
+      else {
+        if(drawCheck()) {
+        $(".winning-message-text").text("Draw!");
+        $("#mail").hide();
+        $(".winning-message").addClass("show");
+        return;
+        }
+      }
+    }
   });
 
+  // Try again button
+  $("#restart-btn").click(function() {
+    $(".winning-message").removeClass("show");
+    reset();
+    $(".field").each(function() {
+      $(this).removeClass("x");
+      $(this).removeClass("o");
+    });
+  });
 
 });
